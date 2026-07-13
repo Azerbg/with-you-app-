0a2176524fb16491159f622c2f990248f4815431
Azer <azer.boughrara@polytechnicien.tn>
2026-07-13 11:58:33 +0100

    feat: M5 L2

---
+++ src/app/find-tutors/layout.tsx
@@ -0,0 +1,5 @@
+import StudentShell from "@/components/StudentShell";
+
+export default function FindTutorsLayout({ children }: { children: React.ReactNode }) {
+  return <StudentShell>{children}</StudentShell>;
+}
